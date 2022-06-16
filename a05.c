e9ea338f8b4e2dddcf773730ec423cc488690e6c
Brennan St. John <101684924+BrennanStJohn@users.noreply.github.com>
2022-06-15 21:04:57 -0500

    figuring out how to set things up and use struct

--- a05.c
+++ a05.c
@@ -3,17 +3,20 @@
 
 struct empData {
 	
-	char firstName;
-	char lastName;
-	int empNumber;
-	int empDept;
-	int empSalary;
+	char firstName[100];
+	char lastName[100];
+	int empNumber[100];
+	int empDept[100];
+	float empSalary[100];
 	
 };
 
+void getRecs(int numOfEmp, FILE *empFile, struct empData);
+void printID(int numOfEmp, int userInput);
+
 int main(int argc, char *argv[]){
 	
-	struct empData;
+	struct empData db;
 	int userInput;
 
 	if (argc != 2) {
@@ -26,11 +29,18 @@ int main(int argc, char *argv[]){
 	
 	empFile = fopen(argv[1], "r");
 	
+	if (empFile == NULL) {
+		
+		printf("The file could not be opened.\n");
+		exit(EXIT_FAILURE);
+	}
+	
 	int numOfEmp;
 	
 	fscanf(empFile, "%d", &numOfEmp);
 	
-	while ( userInput != 0) {
+	while ( userInput != -1) {
+		
 		
 		puts("Welcome to the Employee Database!");
 		puts("---------------------------------");
@@ -38,9 +48,70 @@ int main(int argc, char *argv[]){
 		puts("1:   Print Employee ID");
 		puts("2:   Print ALL Employees");
 		puts("3:   Show all employees in a department");
+		puts("4:   Show salaries from highest to lowest");
 		puts("-1:  QUIT");
 		
+		printf("%s", "Input: ");
+		
 		scanf("%d", &userInput);
+		
+		puts("");
+		
+		if (userInput == 1) {
+		
+		getRecs(numOfEmp, empFile, db);
+		
+		printID(numOfEmp, userInput);
+		
+		}
+		
+		else if (userInput == 2) {
+			
+			/*
+			for (int i = 0; i < numOfEmp; i++) {	
+			fscanf(empFile, "%d%s%s%d%f", &db.empNumber[i], &db.firstName[i], &db.lastName[i], &db.empDept[i], db.empSalary[i]);
+			}
+			*/
+			for(int i = 0; i < numOfEmp; i++){
+			
+			printf("ID: %10s %d\n", db.empNumber[i]);
+			printf("First Name: %10s %s\n", db.firstName[i]);
+			printf("Last Name: %10s %s\n", db.lastName[i]);
+			printf("Department: %10s %d\n", db.empDept[i]);
+			printf("Salary: %10s %.2f\n", db.empSalary[i]);
+			
+			}
+			
+		}
+		else if (userInput == 3) {
+			
+			
+			
+		}
+		
+		else if (userInput == 4) {
+			
+			
+			
+		}
+		else if (userInput == 0) {
+			
+			printf("Please enter a valid number!\n");
+			puts("");
+			
+		}
+		else if (userInput < -1 || userInput > 4) {
+			
+			printf("Please enter a valid number!\n");
+			puts("");
+			
+		}
+		else if (userInput == -1) {
+			
+			printf("Exiting...");
+			break;
+			
+		}
 		
 		
 		
@@ -51,4 +122,53 @@ int main(int argc, char *argv[]){
 		
 	}
 
+	fclose(empFile);
+
+}
+
+void getRecs(int numOfEmp, FILE *empFile, struct empData) {
+
+	struct empData db;
+
+	for (int i = 0; i < numOfEmp; i++) {	
+		fscanf(empFile, "%d%s%s%d%f", &db.empNumber[i], &db.firstName[i], &db.lastName[i], &db.empDept[i], db.empSalary[i]);
+		}
+	
+}
+
+void printID(int numOfEmp, int userInput){
+
+	struct empData db;
+	
+	int ID;
+	
+		printf("%s", "Which employee ID to print? ");
+		scanf("%d", &ID);
+			
+		/*
+		for (int i = 0; i < numOfEmp; i++) {	
+		fscanf(empFile, "%d%s%s%d%f", &db.empNumber[i], &db.firstName[i], &db.lastName[i], &db.empDept[i], db.empSalary[i]);
+		}
+		*/
+			
+		for(int i = 0; i < numOfEmp; i++){
+		
+			if (ID = db.empNumber[i]) {
+				
+				printf("ID: %10s %d\n", db.empNumber[i]);
+				printf("First Name: %10s %s\n", db.firstName[i]);
+				printf("Last Name: %10s %s\n", db.lastName[i]);
+				printf("Department: %10s %d\n", db.empDept[i]);
+				printf("Salary: %10s %.2f\n", db.empSalary[i]);
+				
+			}
+			else {
+				
+				printf("Employee %d not found!", userInput);
+				
+			}
+		
+		}
+			
+			
 }
